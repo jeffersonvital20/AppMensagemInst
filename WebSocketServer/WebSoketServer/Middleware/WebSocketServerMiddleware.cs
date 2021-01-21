5868d51e344a64d470eca324311d4473f6d69ed6
jefferson vital <jeffersonvital.jca@outlook.com>
2021-01-21 18:00:11 -0300

    Enviando o connID para o cliente

--- WebSocketServer/WebSoketServer/Middleware/WebSocketServerMiddleware.cs
+++ WebSocketServer/WebSoketServer/Middleware/WebSocketServerMiddleware.cs
@@ -10,6 +10,7 @@ namespace WebSoketServer.Middleware
     public class WebSocketServerMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly WebSocketServerConnectionManager _manager = new WebSocketServerConnectionManager();
         public WebSocketServerMiddleware(RequestDelegate next)
         {
             _next = next; 
@@ -21,6 +22,9 @@ namespace WebSoketServer.Middleware
                 {
                     WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
                     Console.WriteLine("websocket connected");
+
+                    string ConnID = _manager.AddSocket(webSocket);
+                    await SendConnIDAsync(webSocket, ConnID);
 
                     await ReceiveMessage(webSocket, async (result, buffer) =>
                     {
@@ -40,6 +44,12 @@ namespace WebSoketServer.Middleware
                     await _next(context);
                 }
 
+        }
+
+        private async Task SendConnIDAsync(WebSocket socket, string connID)
+        {
+            var buffer = Encoding.UTF8.GetBytes("ConnID: " + connID);
+            await socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None );
         }
 
         private async Task ReceiveMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
